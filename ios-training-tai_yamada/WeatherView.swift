234c497e2b2eb70f22438107c5138e39d6beb949
山田大陽 <tai09to06y@gmail.com>
2025-10-14 17:51:57 +0900

    不要なモディファイアの整理とTextのButton化（#1）

--- ios-training-tai_yamada/WeatherView.swift
+++ ios-training-tai_yamada/WeatherView.swift
@@ -14,37 +14,33 @@ struct WeatherView: View {
                 Image("dummy")
                     .resizable()
                     .scaledToFit()
-                    .frame(width: geometry.size.width * 0.5) // 幅は画面の半分
-                    .frame(height: geometry.size.width * 0.5) // 高さは幅と同じ
-                    .frame(maxWidth: .infinity)
-                    .clipped()
+                    .frame(width: geometry.size.width * 0.5, height: geometry.size.width * 0.5)
 
                 HStack(spacing: 0) {
                     Text("ー ー")
                         .foregroundColor(.blue)
-                        .frame(width: geometry.size.width * 0.25) // 画像の半分
+                        .frame(width: geometry.size.width * 0.25)
                     Text("ー ー")
                         .foregroundColor(.red)
                         .frame(width: geometry.size.width * 0.25)
                 }
-                .frame(maxWidth: .infinity, alignment: .center)
                 
                 HStack(spacing: 0) {
-                    Text("Close")
-                        .foregroundColor(.blue)
-                        .frame(width: geometry.size.width * 0.25)
-                    
-                    Text("Reload")
-                        .foregroundColor(.blue)
-                        .frame(width: geometry.size.width * 0.25)
-                    
+                    // ボタンの機能は今後のタスクで実装する
+                    Button(action: {}) {
+                        Text("Close")
+                            .foregroundColor(.blue)
+                            .frame(width: geometry.size.width * 0.25)
+                    }
+                    Button(action: {}) {
+                        Text("Reload")
+                            .foregroundColor(.blue)
+                            .frame(width: geometry.size.width * 0.25)
+                    }
                 }
-                .padding(.top, 80) // Labelとの隙間は80pt
-                
+                .padding(.top, 80)
             }
-            .frame(maxHeight: .infinity, alignment: .center) // VStack全体を中央に
-            
-
+            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
         }
     }
 }
