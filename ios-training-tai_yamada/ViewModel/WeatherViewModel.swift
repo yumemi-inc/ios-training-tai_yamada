459f2965378c0bcf8113da916a9ee3e2c4be617a
山田大陽 <tai09to06y@gmail.com>
2025-10-14 15:25:35 +0900

    タップで天気画像と色が変化する機能を実装（#3）

---
+++ ios-training-tai_yamada/ViewModel/WeatherViewModel.swift
@@ -0,0 +1,19 @@
+//
+//  WeatherViewModel.swift
+//  ios-training-tai_yamada
+//
+//  Created by 山田 大陽 on 2025/10/14.
+//
+
+import Combine
+import YumemiWeather
+
+@MainActor
+final class WeatherViewModel: ObservableObject {
+    @Published var weather: String = "unknown"
+    
+    func fetchWeather() {
+        weather = YumemiWeather.fetchWeatherCondition()
+        print("取得した天気: \(weather)")
+    }
+}
